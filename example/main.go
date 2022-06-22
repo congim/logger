1cca5a2144e88c085f9dafe281c658467d5ca9ca
shaocongcong <shao.congcong@yalla.live>
2022-06-22 15:22:05 +0800

    time error

--- example/main.go
+++ example/main.go
@@ -1,56 +1,35 @@
 package main
 
 import (
-	"errors"
 	"time"
 
 	"github.com/congim/logger"
-	"github.com/spf13/pflag"
 )
-
-var (
-	confPath string
-)
-
-func init() {
-	pflag.StringVar(&confPath, "conf", "configs/config.toml", "default configs path")
-}
 
 func main() {
 	c := logger.New()
 	c.SetDivision("time")     // 设置归档方式，"time"时间归档 "size" 文件大小归档，文件大小等可以在配置文件配置
 	c.SetTimeUnit(logger.Day) // 时间归档 可以设置切割单位
 	c.SetEncoding("json")     // 输出格式 "json" 或者 "console"
-	//c.Stacktrace = true
 
-	c.SetInfoFile("./logs/server.log")      // 设置info级别日志
-	c.SetErrorFile("./logs/server_err.log") // 设置warn级别日志
-
-	c.SentryConfig = logger.SentryLoggerConfig{
-		DSN:              "sentry dsn",
-		Debug:            true,
-		AttachStacktrace: true,
-		Environment:      "dev",
-		Tags: map[string]string{
-			"source": "demo",
-		},
-	}
+	c.SetInfoFile("./logs/server.log")  // 设置info级别日志
+	c.SetErrorFile("./logs/server.log") // 设置warn级别日志
 
 	c.InitLogger()
 
-	logger.Info("info level test")
-	logger.Error("dsdadadad level test", logger.WithError(errors.New("sabhksasas")))
-	logger.Error("121212121212 error")
-	logger.Warn("warn level test")
-	logger.Debug("debug level test")
+	for {
+		logger.Info("info level test")
+		logger.Error("error level test")
+		logger.Warn("warn level test")
+		logger.Debug("debug level test")
+		//logger.Fatal("fatal level test")
 
-	time.Sleep(2 * time.Second) // 避免程序结束太快，没有上传sentry
+		// format
+		logger.Infof("info level test: %s", "111")
+		logger.Errorf("error level test: %s", "111")
+		logger.Warnf("warn level test: %s", "111")
+		logger.Debugf("debug level test: %s", "111")
+		time.Sleep(1 * time.Second)
+	}
 
-	logger.Infof("info level test: %s", "111")
-	logger.Errorf("error level test: %s", "111")
-	logger.Warnf("warn level test: %s", "111")
-	logger.Debugf("debug level test: %s", "111")
-
-	logger.Info("this is a log", logger.With("Trace", "12345677"))
-	logger.Info("this is a log", logger.WithError(errors.New("this is a new error")))
 }
--- go.mod
+++ go.mod
@@ -5,14 +5,11 @@ go 1.16
 require (
 	github.com/BurntSushi/toml v1.1.0
 	github.com/getsentry/sentry-go v0.13.0
+	github.com/jonboulle/clockwork v0.3.0 // indirect
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
+	github.com/lestrrat-go/strftime v1.0.6 // indirect
 	github.com/spf13/pflag v1.0.5
 	go.uber.org/zap v1.21.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v2 v2.4.0
-)
-
-require (
-	github.com/jonboulle/clockwork v0.3.0 // indirect
-	github.com/lestrrat-go/strftime v1.0.6 // indirect
 )
